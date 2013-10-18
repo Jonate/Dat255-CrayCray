138886471c3939b393b9a78caa7a293eb4249913
materne <hanna.materne@live.se>
2013-10-18 16:37:49 +0200

    changed databasetestmethod

--- CrayCray/test/se/chalmers/dat255/craycray/databasetest/DatabaseAdapterTest.java
+++ CrayCray/test/se/chalmers/dat255/craycray/databasetest/DatabaseAdapterTest.java
@@ -1,5 +1,7 @@
 package se.chalmers.dat255.craycray.databasetest;
 
+import org.junit.Assert;
+
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 import se.chalmers.dat255.craycray.database.DatabaseAdapter;
@@ -17,13 +19,28 @@ public class DatabaseAdapterTest extends AndroidTestCase{
 	
 	}
 	
-	public void addValueTest() throws DatabaseException{
-	
-		assertTrue(adapter.getValue("test1")==0);
-		//assertTrue(adapter.addValue("test1", 3)==1);
-		//assertTrue(adapter.addValue("test2", 4)==1);
-		//assertTrue(adapter.addValue("test3", 5)==2);
+	public void addValueTest(){
 		
+		//RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_"); 
+		//adapter = DatabaseAdapter.getInstance(context);
+		
+		try{
+			adapter.addValue("test0", 0);
+			}
+		catch(DatabaseException d){}
+		/*try{
+			assertTrue(adapter.addValue("test1", 3)==-1);
+		}
+		catch(DatabaseException d){}
+		try{
+			assertTrue(adapter.addValue("test2", 4)==-1);
+		}
+		catch(DatabaseException d){}
+		try{
+			assertTrue(adapter.addValue("test3", 5)==-1);
+		}
+		catch(DatabaseException d){}*/
+	
 	}
 	
 	public void getValueTest() throws DatabaseException{
